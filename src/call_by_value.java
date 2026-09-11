cc4974c6519e2e2a159f9a4a2fc0a4f358aedd0d
Sakshi Kumari <sakshikri.9472@gmail.com>
2026-09-11 11:29:35 +0530

    commit

---
+++ src/call_by_value.java
@@ -0,0 +1,16 @@
+package src;
+
+public class call_by_value {
+    public static void main(String[] args) {
+        int x=4;
+        int y= 5;
+        System.out.println(x+ " " + y);
+        addTen(x, y);
+        System.out.println(x+ " " + y);
+
+    }
+    static void addTen(int x,int y){
+        x=x+10;
+        y=y+10;
+    }
+}
